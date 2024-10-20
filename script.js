7b3d51b49b3248b4fb958a203a0fdeb6cd016cab
Iflaq khurshid <112414810+Iflaqbhat@users.noreply.github.com>
2024-10-20 08:42:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+  let elem = document.getElementById("status");
+        let button = document.getElementById("enterbtn");
+        button.addEventListener("click", function() {
+            elem.innerHTML = "<h1>Entered Metaverse</h1>";
+        });
